9df17baee68e21cae15026f4c0d26912dd9bc70d
Mark Allison <mark@digital-impacts.org>
2014-12-18 15:36:41 +0000

    Add Feature model for grouping permissions

---
+++ mysite/engage/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    permissions = models.ManyToManyField('auth.Permission')
+
+    class Meta:
+        # TODO put these here for now until we have the right model/app to hang them off
+        permissions = (
+            ('view_post', 'Can view blog post'),
+            ('add_post', 'Can add blog post'),
+            ('change_post', 'Can change blog post'),
+            ('delete_post', 'Can delete blog post'),
+            ('moderate_post', 'Can moderate blog post'),
+            
+            ('can_message', 'Can send message'),
+        )
