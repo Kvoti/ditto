6e8e22d72035534d4f59a06b575c50d3c75ea529
Mark Allison <mark@digital-impacts.org>
2015-02-05 17:27:17 +0000

    Refator room creation js with Promise

--- mysite/ditto/static/ditto/newroom.js
+++ mysite/ditto/static/ditto/newroom.js
@@ -6,51 +6,70 @@ $(document).ready(function () {
     $('form').submit(function (e) {
         e.preventDefault();
         var form = $(this);
-        var participants = form.find('option:selected')
+        var members = form.find('option:selected')
             .map(function () {
                 return $(this).text();
             }).get();
 
         name = $(this.name).val();
 
-        if (name && participants) {
-            createRoom(name, participants);
+        if (name && members) {
+	    createRoom(name, members)
+		.then(configureRoom)
+		.then(addMembers)
+		.then(loadNewRoom);
         } else {
-            alert("Please choose a name and some participants!");
+            alert("Please choose a name and some members!");
         }
     });
-
-    function createRoom(name, participants) {
-        room_jid = name + '@muc.' + DITTO.chat_host;
-        connection.muc.join(room_jid, DITTO.chat_nick, null,
-                            function () { configureRoom(participants); }
-                           );
+    
+    function createRoom(name, members) {
+	return new Promise(function (resolve, reject) {
+            room_jid = name + '@muc.' + DITTO.chat_host;
+            connection.muc.join(room_jid, DITTO.chat_nick, null,
+				function () { resolve(members); }
+                               );
+	});
+    }
+    
+    function configureRoom(members) {
+	return new Promise(function (resolve, reject) {
+            connection.muc.createConfiguredRoom(
+		room_jid,
+		{'muc#roomconfig_membersonly': 1},
+		function () { resolve(members); },
+		function () { reject(Error('oops')); }
+            );
+	});
     }
 
-    function configureRoom(participants) {
-        connection.muc.createConfiguredRoom(
-            room_jid,
-            {'muc#roomconfig_membersonly': 1},
-            function () { addMembers(participants); }
-        );
-    }
-
-    function addMembers(participants) {
-	var callback = function () { };
-        $.each(participants, function (i, participant) {
-	    if (participants[participants.length - 1] === participant) {
-		callback = function () {
-		    window.location.href = '../' + name + '/';
-		};
-	    }
-            connection.muc.member(room_jid, participant + '@' + DITTO.chat_host, "", callback);
+    function addMembers(members) {
+	var ps = $.map(members, function (member) {
+	    addMember(member);
         });
+	return Promise.all(ps);
     }
+
+    function addMember(member) {
+	return new Promise(function (resolve, reject) {
+            connection.muc.member(
+		room_jid,
+		member + '@' + DITTO.chat_host,
+		"",
+		function () { console.log('added', member);resolve(); }
+	    );
+	});
+    }
+	    
+    function loadNewRoom() {
+	console.log('everyone added');
+	window.location.href = '../' + name + '/';
+    };
 
     function connect () {
 	connection = new Strophe.Connection('ws://' + DITTO.chat_domain + ':5280/ws-xmpp');
-	// connection.rawInput = rawInput;
-	// connection.rawOutput = rawOutput;
+	connection.rawInput = rawInput;
+	connection.rawOutput = rawOutput;
 	connection.connect(
 	    DITTO.chat_name, DITTO.chat_pass, onConnect
 	);
