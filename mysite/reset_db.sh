0ee26660a374f2972d67d559308953c6ad4f31f1
Mark Allison <mark@digital-impacts.org>
2015-02-12 17:17:57 +0000

    Create example network locally

--- mysite/reset_db.sh
+++ mysite/reset_db.sh
@@ -2,3 +2,5 @@ mysqladmin drop mysite
 mysqladmin create mysite
 python manage.py migrate
 python manage.py runscript setup_test_data
+DJANGO_TENANT=di python manage.py migrate
+DJANGO_TENANT=di python manage.py runscript setup_test_data
