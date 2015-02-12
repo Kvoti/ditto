de6cb478e09b61b36ab62d46fb1d293fc40d90c6
Mark Allison <mark@digital-impacts.org>
2015-02-12 14:08:14 +0000

    Tiny refactor using new utils

--- mysite/multitenancy/forms.py
+++ mysite/multitenancy/forms.py
@@ -1,17 +1,19 @@
 import floppyforms.__future__ as forms
 from slugify import slugify
 
+import utils
+
 from . import models
 
 
-class TenantForm(forms.ModelForm):
+class TenantForm(utils.forms.UserModelFormMixin, forms.ModelForm):
     class Meta:
         model = models.Tenant
         fields = ('network_name',)
         
-    def __init__(self, user, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(TenantForm, self).__init__(*args, **kwargs)
-        self.instance.user = user
+        utils.forms.autofocus(self, 'network_name')
         
     def save(self, *args, **kwargs):
         self.instance.slug = slugify(self.cleaned_data['network_name'])
