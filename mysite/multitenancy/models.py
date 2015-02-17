76ebe2189af2c96416b8654a15fca85fe312fff8
Mark Allison <mark@digital-impacts.org>
2015-02-17 14:37:02 +0000

    Fix network url

--- mysite/multitenancy/models.py
+++ mysite/multitenancy/models.py
@@ -36,4 +36,4 @@ class Tenant(models.Model):
             # Do the Site import here to avoid messing up the
             # monkeypatching of _meta.db_table
             from django.contrib.sites.models import Site
-            return 'http://%s/%s/' % (self.slug, Site.objects.get_current())
+            return 'http://%s/%s/' % (Site.objects.get_current(), self.slug)
