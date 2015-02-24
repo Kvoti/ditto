403bbaf3fc17641142590dfaf115652ab04cbfcf
Mark Allison <mark@digital-impacts.org>
2015-02-24 11:11:35 +0000

    WIP enforce chat permissions

--- ditto/chat/views.py
+++ ditto/chat/views.py
@@ -1,10 +1,12 @@
 from braces.views import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.core.signing import Signer
+from django.http import Http404
 from django.template.response import TemplateResponse
 from django.views.generic import TemplateView, ListView, DetailView
 
 
+import configuration.utils
 from core.views.decorators import extra_context, nav
 from core.views.mixins import NavMixin
 from users.models import User
@@ -61,6 +63,11 @@ class PrivateChatsView(LoginRequiredMixin, NavMixin, ListView):
     template_name = 'chat/private_chats.html'
     nav = ['messages']
 
+    def get_queryset(self):
+        return configuration.utils.get_permitted_users_for_messaging(
+            self.request.user,
+        )
+
     
 class PrivateChatView(LoginRequiredMixin, NavMixin, ChatAuthMixin, DetailView):
     model = User
@@ -68,3 +75,12 @@ class PrivateChatView(LoginRequiredMixin, NavMixin, ChatAuthMixin, DetailView):
     context_object_name = 'chatee'
     template_name = 'chat/private_chat.html'
     nav = ['private_chat']
+
+    def get_object(self, queryset=None):
+        obj = super(PrivateChatView, self).get_object(queryset)
+        if configuration.utils.is_user_messaging_permitted(
+                self.request.user,
+                obj
+        ):
+            return obj
+        raise Http404
--- ditto/configuration/utils.py
+++ ditto/configuration/utils.py
@@ -1,4 +1,9 @@
 from django.contrib.auth.models import Group
+from django.db.models import Q
+
+from users.models import User
+
+from . import models
 
 
 def get_role_grid():
@@ -9,3 +14,34 @@ def get_role_grid():
         if roles:
             grid.append((role, roles[::]))
     return grid
+
+
+def get_permitted_users_for_messaging(user):
+    return get_permitted_users_for_interaction(user, 'Messaging')
+
+
+def get_permitted_users_for_interaction(user, interaction):
+    role = _get_role(user)
+    permitted = User.objects.filter(
+        Q(groups__permitted_interactions_1__interaction__name=interaction,
+          groups__permitted_interactions_1__role2=role) |
+        Q(groups__permitted_interactions_2__interaction__name=interaction,
+          groups__permitted_interactions_2__role1=role)
+    )
+    return permitted
+
+
+def is_user_messaging_permitted(user1, user2):
+    return is_user_interaction_permitted('Messaging', user1, user2)
+
+
+def is_user_interaction_permitted(interaction, user1, user2):
+    role1 = _get_role(user1)
+    role2 = _get_role(user2)
+    return models.Interaction.objects.get(
+        name=interaction).is_permitted(role1, role2)
+
+
+def _get_role(user):
+    # TODO not sure here if user will have multiple groups
+    return user.groups.all()[0]
