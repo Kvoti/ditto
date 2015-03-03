59066618ad5f5a154176c9d68948947e3337af3b
Mark Allison <mark@digital-impacts.org>
2015-03-03 15:05:40 +0000

    Fix profile avatar rendering

--- ditto/static/chat/js/avatars.js
+++ ditto/static/chat/js/avatars.js
@@ -1,6 +1,8 @@
 // TODO move all avatar related stuff in to here
-$('div[data-avatar]').each(function () {
-    var div = $(this);
-    var avatar = DITTO.chat.getAvatar(div.data('name'), div.data('size'));
-    div.append(avatar);
+$(document).on('connected.ditto.chat', function (e, conn) {
+    $('div[data-avatar]').each(function () {
+        var div = $(this);
+        var avatar = DITTO.chat.getAvatar(div.data('name'), div.data('size'));
+        div.append(avatar);
+    });
 });
