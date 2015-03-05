829747ef75bcf26cf73ef127cab79b030ca0b04b
Mark Allison <mark@digital-impacts.org>
2015-03-05 09:26:21 +0000

    Load 20 most recent pchats

--- ditto/static/chat/js/mam.js
+++ ditto/static/chat/js/mam.js
@@ -5,28 +5,26 @@
     var connection;
 
     $(document).on('connected.ditto.chat', function (e, conn) {
-        var an_hour_ago = new Date();
-        an_hour_ago.setHours(an_hour_ago.getHours() - 1);
         connection = conn;
 	connection.mam.init(connection);
 	connection.mam.query(
 	    Strophe.getBareJidFromJid(DITTO.chat_name),
 	    {
 		'with': Strophe.getBareJidFromJid(DITTO.chatee),
-                'start': an_hour_ago.toISOString(),
-                // 'max': 10,
+                'before': "",
+                'max': 20,
 		onMessage: onArchivedPrivateMessage
 	    }
 	);
     });
     
     function onArchivedPrivateMessage(msg) {
-	var msg = $(msg);
-	var body = msg.find("body:first").text();
-	var from = msg.find('message').attr("from").split('@')[0];
+        var msg = $(msg);
+        var body = msg.find("body:first").text();
+        var from = msg.find('message').attr("from").split('@')[0];
         var when = new Date(msg.find('delay').attr('stamp'));
-	DITTO.chat.renderPrivateMessage(from, when, body);
-	return true;
+        DITTO.chat.renderPrivateMessage(from, when, body);
+        return true;
     }
 
 })();
