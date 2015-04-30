18a2f4f9815fb4f2ff6754dbb5f20045d4de20b0
Mark Allison <mark@digital-impacts.org>
2015-04-30 09:49:32 +0100

    Fix main ditto signup form

--- ditto/signup/forms.py
+++ ditto/signup/forms.py
@@ -7,12 +7,14 @@ from dittoforms.utils import FormFromSpecMixin
 
 
 class SignupForm(FormFromSpecMixin, forms.Form):
-    def __init__(self, role, *args, **kwargs):
-        self.role = role
-        spec = FormSpec.objects.get(regform__role=role)
-        self.spec = spec
+    def __init__(self, role=None, *args, **kwargs):
         super(SignupForm, self).__init__(*args, **kwargs)
-        self.add_fields_from_spec(json.loads(spec.spec))
+        if role is not None:
+            self.role = role
+            spec = FormSpec.objects.get(regform__role=role)
+            self.spec = spec
+            self.add_fields_from_spec(json.loads(spec.spec))
 
     def signup(self, request, user):
-        self.save_submission(self.spec, user)
+        if hasattr(self, 'role'):
+            self.save_submission(self.spec, user)
--- ditto/signup/views.py
+++ ditto/signup/views.py
@@ -2,22 +2,30 @@ from allauth.account.views import SignupView as BaseSignupView
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import Group
 
+import core
+
+MEMBER = core.MEMBER_ROLE
+
 
 class SignupView(BaseSignupView):
     def dispatch(self, request, role_name=None):
-        if role_name is None:
-            role_name = 'Member'
-        self.role = get_object_or_404(Group, name=role_name)
+        if not request.tenant.is_main():
+            if role_name is None:
+                role_name = MEMBER
+            self.role = get_object_or_404(Group, name=role_name)
         return super(SignupView, self).dispatch(request, role_name)
         
     def get_form_kwargs(self):
         kwargs = super(SignupView, self).get_form_kwargs()
-        kwargs['role'] = self.role
+        if not self.request.tenant.is_main():
+            kwargs['role'] = self.role
         return kwargs
 
     def get_context_data(self, **kwargs):
         context = super(SignupView, self).get_context_data(**kwargs)
-        context['roles'] = Group.objects.exclude(name='Member').values_list('name', flat=True)
+        if not self.request.tenant.is_main():
+            context['roles'] = Group.objects.exclude(
+                name=MEMBER).values_list('name', flat=True)
         return context
     
 signup = SignupView.as_view()
