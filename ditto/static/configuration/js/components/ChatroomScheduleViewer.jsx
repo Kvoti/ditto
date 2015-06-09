114071de5a80e3aafda082c898e9b911a7ecd97d
Mark Allison <mark@digital-impacts.org>
2015-06-09 12:32:03 +0100

    refactor viewer

--- ditto/static/configuration/js/components/ChatroomScheduleViewer.jsx
+++ ditto/static/configuration/js/components/ChatroomScheduleViewer.jsx
@@ -1,6 +1,7 @@
 var React = require('react/addons');
 var update = React.addons.update;
 var Constants = require('../constants/SettingsConstants');
+var assign = require('object-assign');
 
 var ChatroomScheduleViewer = React.createClass({
 
@@ -17,37 +18,21 @@ var ChatroomScheduleViewer = React.createClass({
     render () {
 	var rows = [for (d of Constants.days) [this._renderDayLabel(d)]];
 	this.props.slots.forEach(s => {
-	    var width;
-	    var offset;
 	    var end;
-	    var day = rows[Constants.days.indexOf(s.day)];
-	    var nextDay = rows[(Constants.days.indexOf(s.day) + 1) % 7];
-	    offset = this.props.width * s.start / 24 + this.props.dayLabelWidth;
+	    var dayIndex = Constants.days.indexOf(s.day);
+	    var day = rows[dayIndex];
+	    var nextDay = rows[(dayIndex + 1) % 7];
 	    if (s.end < s.start) {
 		end = 24;
 	    } else {
 		end = s.end
 	    }
-	    width = this.props.width * (end - s.start) / 24;
 	    day.push(
-		<div style={{
-			    position: 'absolute',
-			    left: offset,
-			    width: width,
-			    height: this.props.rowHeight,
-			    backgroundColor: 'blue'
-			    }}></div>
+		<Slot dayLabelWidth={this.props.dayLabelWidth} rowWidth={this.props.width} start={s.start} end={end} />
 	    );
-	    width = this.props.width * s.end / 24;
 	    if (s.end < s.start) {
 		nextDay.push(
-		    <div style={{
-				position: 'absolute',
-				left: this.props.dayLabelWidth,
-				width: width,
-				height: this.props.rowHeight,
-				backgroundColor: 'blue'
-				}}></div>
+		    <Slot dayLabelWidth={this.props.dayLabelWidth} rowWidth={this.props.width} start={0} end={s.end} />
 		);
 	    }
 	});
@@ -55,19 +40,7 @@ var ChatroomScheduleViewer = React.createClass({
 	return (
 	    <div>
 		{this._renderHeader()}
-		{rows.map(row => {
-		    return (
-			<div style={{
-			    clear: 'both',
-			    width: this.props.width,
-			    height: this.props.rowHeight,
-			    marginTop: 3,
-			    position: 'relative'
-			}}>
-			{row}
-			</div>
-		    );
-		 })}
+		{rows.map(row => <Row width={this.props.width} height={this.props.rowHeight}>{row}</Row>)}
 	    </div>
 	);
     },
@@ -108,17 +81,72 @@ var ChatroomScheduleViewer = React.createClass({
 	    );
 	}
 	return (
-	    <div style={{
-			clear: 'both',
-			width: this.props.width,
-			height: this.props.rowHeight,
-			marginLeft: -0.5 / 24 * this.props.width + this.props.dayLabelWidth,
-			}}>
-		{labels}
-	    </div>
+	    <Row width={this.props.width} height={this.props.rowHeight} style={{
+		marginLeft: -0.5 / 24 * this.props.width + this.props.dayLabelWidth,
+	    }}>
+	    {labels}
+	    </Row>
 	);
     }
     
+});
+
+var Row = React.createClass({
+    render () {
+	var defaultStyle = {
+	    clear: 'both',
+	    width: this.props.width,
+	    height: this.props.height,
+	    marginTop: 3,
+	    position: 'relative',
+	};
+	var style = assign(defaultStyle, this.props.style);
+	return (
+	    <div style={style}>
+		{this.props.children}
+	    </div>
+	);
+    }
+});
+
+var Slot = React.createClass({
+    // TODO had a quick go at detailed validation, needs a fair bit of tidy up
+    // (better error messages, checking start/end are integers etc.)
+    propTypes: {
+	rowWidth: React.PropTypes.number.isRequired,
+	dayLabelWidth: React.PropTypes.number.isRequired,
+	start (props, propName, componentName) {
+	    var value = props[propName];
+	    if (value < 0 || value > 23) {
+		return new Error('Start time must be 0..23');
+	    }
+	},
+	end (props, propName, componentName) {
+	    var value = props[propName];
+	    if (value < 1 || value > 24) {
+		return new Error('Start time must be 1..24');
+	    }
+	    var start = props.start;
+	    if (value < start + 1) {
+		return new Error('End must be greater than start');
+	    }
+	}
+    },
+
+    render () {
+	var offset = this.props.rowWidth * this.props.start / 24 + this.props.dayLabelWidth;
+	var width = this.props.rowWidth * (this.props.end - this.props.start) / 24;
+	return (
+	    <div style={{
+			position: 'absolute',
+			left: offset,
+			width: width,
+			height: '100%',
+			backgroundColor: 'blue'
+			}}>
+	    </div>
+	);
+    }
 });
 
 module.exports = ChatroomScheduleViewer;
