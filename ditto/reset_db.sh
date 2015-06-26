3df331fdab34dc49e1e4574189e447ff87c182aa
Mark Allison <mark@digital-impacts.org>
2015-06-26 12:14:46 +0100

    Attempt to have reset_db destroy all chat data too

--- ditto/reset_db.sh
+++ ditto/reset_db.sh
@@ -1,8 +1,11 @@
 mysqladmin -uroot drop ditto
 mysqladmin -uroot create ditto
-#echo 'source /usr/local/lib/mongooseim/lib/ejabberd-2.1.8+mim-1.5.0/priv/mysql.sql' | python manage.py dbshell
+mysqladmin -uroot drop chat
+mysqladmin -uroot create chat
+echo 'source /usr/local/lib/mongooseim/lib/ejabberd-2.1.8+mim-1.5.0/priv/mysql.sql' | mysql -uroot chat
 python manage.py migrate
 python manage.py runscript setup_test_data
 DJANGO_TENANT=di python manage.py migrate
 DJANGO_TENANT=di python manage.py runscript setup_test_data
 #DJANGO_TENANT=di python manage.py runscript setup_chat_data
+sudo mongooseimctl restart
