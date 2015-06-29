6a94783e6c7313127314a52ceac543b91cb25df8
Mark Allison <mark@digital-impacts.org>
2015-06-29 09:59:11 +0100

    Hacky tenant fix to prevent 500 error on rendering 404

--- ditto/multitenancy/middleware.py
+++ ditto/multitenancy/middleware.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.http import Http404
 from django.contrib.sites.models import Site
 from . import tenant
@@ -16,11 +18,14 @@ class CurrentTenantMiddleware(object):
         Site.objects.clear_cache()
         
     def process_response(self, request, response):
+        logging.debug('unsetting tenant in response')
         self._unset()
         return response
 
-    def process_exception(self, request, exception):
-        self._unset()
+    # Take this out for now as it causes an error when trying to render a 404
+    # def process_exception(self, request, exception):
+    #     logging.debug('unsetting tenant in exception response')
+    #     self._unset()
 
     def _unset(self):
         try:
