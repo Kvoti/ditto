c26d6f91fa94ae3773ee472e893fadcda6b774e5
Mark Allison <mark@digital-impacts.org>
2015-07-07 11:55:57 +0100

    Fix home page

--- ditto/configuration/templatetags/configuration.py
+++ ditto/configuration/templatetags/configuration.py
@@ -12,12 +12,3 @@ def theme():
     except IndexError:
         theme = ""
     return theme
-
-
-@register.assignment_tag(takes_context=True)
-def features(context):
-    # TODO features probably need ordering
-    features = models.Feature.objects.filter(is_active=True)
-    if not context['user'].has_perm('configuration.can_chat'):
-        features = features.exclude(slug='chatroom')
-    return features
