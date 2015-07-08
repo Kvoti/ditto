4326b1285e85eaad2fb37083f78a3ea16fc5d314
Mark Allison <mark@digital-impacts.org>
2015-07-08 17:32:09 +0100

    Add placeholder ticket and safeguarding tables

--- ditto/dashboard/views.py
+++ ditto/dashboard/views.py
@@ -1,8 +1,57 @@
-from core.views.mixins import NavMixin
+import datetime
+import random
 
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from core.views.decorators import admin_required
+from core.views.mixins import NavMixin
+from users.models import User
 
+
+#TODO permission required
 class DashView(NavMixin, TemplateView):
     template_name = 'dashboard/dashboard.html'
     nav = ['dashboard']
+
+    
+@admin_required
+def tickets(request):
+    types = [
+        'Blog moderation',
+        'Book appointment',
+        'Case note shared',
+        'Request for help',
+        'Comment moderation',
+    ]
+    return _tickets(request, types)
+
+
+@admin_required
+def safeguarding(request):
+    types = [
+        'Internal referral',
+        'External referral',
+        'Session note shared',
+        'Unplanned ending',
+        'Ban',
+        'Warning',
+    ]
+    return _tickets(request, types)
+
+
+def _tickets(request, types):
+    users = User.objects.all()
+    tickets = []
+    date = datetime.datetime(2015, 7, 8, 15, 30)
+    for i in range(30):
+        date += datetime.timedelta(minutes=random.choice([5, 15, 30, 60, 180]))
+        tickets.append(dict(
+            client=random.choice(users).username,
+            professional=random.choice(users).username,
+            type=random.choice(types),
+            created=date
+        ))
+    return render(request, 'dashboard/tickets.html', {
+        'tickets': tickets
+    })
