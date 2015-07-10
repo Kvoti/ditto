e99177b5149cc9296fb175456a10bd4e6f141e2c
Mark Allison <mark@digital-impacts.org>
2015-07-10 09:29:57 +0100

    Remove unused import

--- ditto/tickets/api/views.py
+++ ditto/tickets/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import generics, response, status
+from rest_framework import generics, response
 from rest_framework.decorators import api_view
 
 from .. import models
