c3085feeb6352166b5d24b13bf11fe24331d7950
Mark Allison <mark@digital-impacts.org>
2015-07-23 12:47:20 +0100

    Switch to FixedDataTable for case notes

--- ditto/static/tidy/js/casenotes/CaseNotesViewer.jsx
+++ ditto/static/tidy/js/casenotes/CaseNotesViewer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import DataGrid from 'react-datagrid';
+import { Table, Column } from 'fixed-data-table';
+import TimeAgo from '../../../flux-chat/js/components/TimeAgo.react';
 
 import commonPropTypes from './commonPropTypes';
 
@@ -8,19 +9,46 @@ export default class CaseNotesViewer extends React.Component {
 	caseNotes: commonPropTypes.caseNotes
     }
 
-    render () {
-	return (
-	    <DataGrid
-		    idProperty="caseNotesTable"
-		    dataSource={this.props.caseNotes}
-		    columns={[
-			     {name: 'author'},
-			     {name: 'created_at'},
-			     {name: 'title'},
-			     {name: 'status'},
-			     ]}
-		    />
-	);
+    _rowGetter = (index) => {
+	return this.props.caseNotes[index];
+    }
+
+    _renderDateTime = (dateTime) => {
+	return <TimeAgo when={dateTime} />;
     }
     
+    render () {
+	return (
+	    <Table
+		    rowHeight={50}
+		    rowGetter={this._rowGetter}
+		    rowsCount={this.props.caseNotes.length}
+		    width={600}
+		    maxHeight={600}
+		    headerHeight={50}>
+		<Column
+			label="AUTHOR"
+			width={150}
+			dataKey="author"
+			/>
+		<Column
+			label="CREATED"
+			width={150}
+			dataKey="created_at"
+			cellRenderer={this._renderDateTime}
+			/>
+		<Column
+			label="TITLE"
+			width={150}
+			dataKey="title"
+			/>
+		<Column
+			label="STATUS"
+			width={150}
+			dataKey="status"
+			/>
+	    </Table>
+	);
+    }
+
 }
