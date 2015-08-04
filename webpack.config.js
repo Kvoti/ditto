f618d07e66c27fdbc9b599ccdca0016cb3c2dcc8
Mark Allison <mark@digital-impacts.org>
2015-08-04 12:07:17 +0100

    Fix webpack to load jsx files and fix hot loading by separating out redux-rest

--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,42 @@ var BundleTracker = require('webpack-bundle-tracker');
 var path = require("path");
 
 module.exports = {
-    context: __dirname,
-    
-    entry: [
-        'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
-        'webpack/hot/only-dev-server',
-        './ditto/static/chat/js/base.js',
-    ],
-    
-    output: {
-        path: path.resolve('./ditto/static/bundles/'),
-        filename: "[name]-[hash].js",
-        publicPath: 'http://localhost:3000/assets/bundles/', // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
-    },
+  context: __dirname,
+  
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server',
+    './ditto/static/chat/js/base.js',
+  ],
+  
+  output: {
+    path: path.resolve('./ditto/static/bundles/'),
+    filename: "[name]-[hash].js",
+    publicPath: 'http://localhost:3000/assets/bundles/', // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
+  },
 
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loaders: ['react-hot', "babel-loader?optional[]=es7.comprehensions&optional[]=es7.classProperties&optional[]=es7.objectRestSpread&optional[]=es7.decorators"],
-            }
-        ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleTracker({filename: './webpack-stats.json'}),
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ["react-hot", "babel-loader?optional[]=es7.comprehensions&optional[]=es7.classProperties&optional[]=es7.objectRestSpread&optional[]=es7.decorators"],
+        include: [path.join(__dirname, 'ditto/static')]
+      },
+      // installing a dev package with npm link breaks hot loader, not sure why. So split it out here to main code above can still be hot loaded
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader?optional[]=es7.comprehensions&optional[]=es7.classProperties&optional[]=es7.objectRestSpread&optional[]=es7.decorators"],
+        include: [path.join(__dirname, '../lib/redux-rest')]
+      }
     ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new BundleTracker({filename: './webpack-stats.json'}),
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
 };
