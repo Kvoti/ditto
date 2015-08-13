0c9b09e977f783058bf096dbcf07dbea4b436d41
Mark Allison <mark@digital-impacts.org>
2015-08-13 13:58:08 +0100

    Only display Add button if canAdd

--- ditto/static/tidy/js/lib/schema/proxies/ArrayManager.js
+++ ditto/static/tidy/js/lib/schema/proxies/ArrayManager.js
@@ -56,6 +56,9 @@ export class ArrayManager extends BaseCollectionManager {
   }
 
   canAdd() {
+    if (this.options.canAdd === undefined) {
+      return false;
+    }
     if (this.options.maxLength === undefined) {
       return true;
     }
