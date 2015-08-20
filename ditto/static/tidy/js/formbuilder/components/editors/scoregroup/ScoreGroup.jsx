b95203a4ec6def82d1fdf60c28a8d779e6c5655e
Mark Allison <mark@digital-impacts.org>
2015-08-20 10:47:28 +0100

    Split ScoreGroup editor into smaller methods

--- ditto/static/tidy/js/formbuilder/components/editors/scoregroup/ScoreGroup.jsx
+++ ditto/static/tidy/js/formbuilder/components/editors/scoregroup/ScoreGroup.jsx
@@ -3,7 +3,7 @@ import Sortable from 'react-components/Sortable';
 
 import Question from './Question';
 import Row from './Row';
-import Col from './Col';
+import Column from './Col';
 import Label from './Label';
 import Score from './Score';
 import Input from './Input';
@@ -15,102 +15,131 @@ export default class ScoreGroup extends React.Component {
   render() {
     let spec = this.props.question;
     let textColSize = Math.max(
-      ...spec.scoregroup.items.members.map(
-        m => Math.sqrt(String(m[1].text.get()).length) * 45 + 30
-      )
+            ...spec.scoregroup.items.members.map(
+              m => Math.sqrt(String(m[1].text.get()).length) * 45 + 30
+            )
     );
     let labelColSizes = spec.scoregroup.labels.members.map(l =>
       Math.sqrt(String(l[1].label.get()).length) * 40 + 60
     );
+    let { labels, items } = spec.scoregroup;
     return (
       <div>
         <Question question={spec.question} isRequired={spec.isRequired} />
         <Row>
-          <Col style={{width: textColSize}}></Col>
-          <Sortable
-                  className='scoregroup-labels'
-                  components={
-                             spec.scoregroup.labels.members.map(([i, label]) => {
-                               return (
-                                 <Col
-                                 style={{width: labelColSizes[i]}}
-                                 key={i}
-                                 draggable={true}
-                                 orderingIndex={label.key}
-                                 >
-                                 <Label label={label} />
-                                 </Col>
-                               );
-                               })}
-                onReorder={components => {
-                           spec.scoregroup.labels.reorder([for (c of components) c.props.orderingIndex])
-                           }}
-                  />
-           <Col>
-           {spec.scoregroup.labels.canAdd() ?
-            <AddButton
-            onClick={(e) => spec.scoregroup.labels.add()}
-            >
-            Add label
-            </AddButton>
-            : null
-            }
-          </Col>
+          <Column
+                  style={{width: textColSize}}
+                  >
+          </Column>
+          {this._renderLabels(labels, labelColSizes)}
+          {labels.canAdd() ? this._renderAddLabel(labels) : null}
         </Row>
-        <Sortable
-                components={
-                            spec.scoregroup.items.members.map(([i, item]) => {
-                              return (
-                                <Row key={i} draggable={true} orderingIndex={item.key}>
-                                <Col style={{width: textColSize}}>
-                                <Input
-                                errors={item.text.errors}
-                                value={item.text.get()}
-                                onChange={(e) => item.text.set(e.target.value)}
-                                />
-                                </Col>
-                                {item.scores.members.map(([j, score]) => {
-                                  return (
-                                    <Col
-                                    style={{width: labelColSizes[j]}}
-                                    key={item.text.get() + j}
-                                    centered={true}
-                                    >
-                                    <Score
-                                    score={score} 
-                                    name={item.text.get()}
-                                    />
-                                    </Col>
-                                  );
-                                })}
-                                <Col>
-                                {item.canRemove() ?
-                                  <RemoveButton
-                                  onClick={item.remove}
-                                  ariaLabel="Remove item"
-                                  title="Remove item"
-                                  />
-                                  : null
-                                }
-                                </Col>
-                                </Row>
-                              );
-                            })
-                            }
-                onReorder={components => {
-                           spec.scoregroup.items.reorder([for (c of components) c.props.orderingIndex])
-                           }}
-        />
-        {spec.scoregroup.items.canAdd() ?
-         <p>
-         <AddButton
-         onClick={(e) => spec.scoregroup.items.add()}
-         >
-         Add item
-         </AddButton>
-         </p>
-         : null}
+        {this._renderItems(items, textColSize, labelColSizes)}
+        {items.canAdd() ? this._renderAddItem(items) : null}
       </div>
     );
+  }
+
+  _renderLabels(labels, labelColSizes) {
+    let labelCols = labels.members.map(([i, label]) => {
+      return (
+        <Column
+                style={{width: labelColSizes[i]}}
+                key={i}
+                draggable={true}
+                orderingIndex={label.key}
+                >
+          <Label label={label} />
+        </Column>
+      );
+    });
+    return (
+      <Sortable
+              className='scoregroup-labels'
+              components={labelCols}
+              onReorder={this._reorder.bind(this, labels)}
+      />
+    );
+  }
+
+  _renderAddLabel(labels) {
+    return (
+      <Column>
+        <AddButton
+                onClick={() => labels.add()}
+                >
+          Add label
+        </AddButton>
+      </Column>
+    );
+  }
+
+  _renderItems(items, textColSize, labelColSizes) {
+    let itemRows = items.members.map(([i, item]) => {
+      return (
+        <Row key={i} draggable={true} orderingIndex={item.key}>
+          <Column
+                  style={{width: textColSize}}
+                  >
+            <Input
+                    errors={item.text.errors}
+                    value={item.text.get()}
+                    onChange={(e) => item.text.set(e.target.value)}
+            />
+          </Column>
+          {item.scores.members.map(([j, score]) => {
+            return this._renderItemScore(item, score, labelColSizes);
+           })}
+          <Column>
+            {item.canRemove() ? this._renderItemRemove(item) : null}
+          </Column>
+        </Row>
+      );
+    });
+    return (
+      <Sortable
+              components={itemRows}
+              onReorder={this._reorder.bind(this, items)}
+      />
+    );
+  }
+
+  _renderAddItem(items) {
+    return (
+      <AddButton
+              onClick={() => items.add()}
+              >
+        Add item
+      </AddButton>
+    );
+  }
+
+  _renderItemScore(item, score, labelColSizes) {
+    return (
+      <Column
+              style={{width: labelColSizes[score.key]}}
+              key={item.text.get() + score.key}
+              centered={true}
+              >
+        <Score
+                score={score}
+                name={item.text.get()}
+        />
+      </Column>
+    );
+  }
+
+  _renderItemRemove(item) {
+    return (
+      <RemoveButton
+              onClick={item.remove}
+              ariaLabel="Remove item"
+              title="Remove item"
+      />
+    );
+  }
+
+  _reorder(itemsManager, reorderedComponents) {
+    itemsManager.reorder([for (c of reorderedComponents) c.props.orderingIndex]);
   }
 }
