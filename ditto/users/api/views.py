4ccf6cb7f02e11664b9899abd7e75557b75cb1ab
Mark Allison <mark@digital-impacts.org>
2015-10-30 15:38:02 +0000

    Only you or privileged user can change profile via api

--- ditto/users/api/views.py
+++ ditto/users/api/views.py
@@ -1,14 +1,15 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated        
+from rest_framework.permissions import IsAuthenticated
 
 from .. import models
 from . import serializers
+from .permissions import IsMeOrAdminOrReadOnly
 
 
 class UserList(generics.ListAPIView):
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsMeOrAdminOrReadOnly)
 
 
 class UserDetail(generics.RetrieveUpdateAPIView):
@@ -16,4 +17,4 @@ class UserDetail(generics.RetrieveUpdateAPIView):
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializer
     lookup_field = 'username'
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsMeOrAdminOrReadOnly)
