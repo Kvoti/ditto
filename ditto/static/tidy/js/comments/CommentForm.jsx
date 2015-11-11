7e617b2467e008d05bff792a442d887536745f54
Mark Allison <mark@digital-impacts.org>
2015-11-11 12:30:11 +0000

    Fix #45 Case note comments broken

--- ditto/static/tidy/js/comments/CommentForm.jsx
+++ ditto/static/tidy/js/comments/CommentForm.jsx
@@ -1,39 +1,72 @@
 import React from 'react';
-import Validate from '../lib/form/Validate.jsx';
+import DelayedInput from '../lib/form/DelayedInput';
+import * as schema from 'data-schema/src/schema';
+import ControlErrors from 'react-form-builder/src/components/editors/renderer/ControlErrors';
+import ControlValidationIcon from 'react-form-builder/src/components/editors/renderer/ControlValidationIcon';
+import { controlRowErrorClassNames } from 'react-form-builder/src/components/editors/renderer/utils';
 
 export default class CommentForm extends React.Component {
-    static propTypes = {
-	onSubmit: React.PropTypes.func.isRequired,
-    }
+  static propTypes = {
+    onSubmit: React.PropTypes.func.isRequired,
+  }
 
-    state = {
-	comment: ""
-    }
-    
-    render () {
-	return (
-	    <form onSubmit={this._onSubmit}>
-		<div className="form-group">
-		    <Validate isRequired={true} id="comment">
-			<textarea
-				className="form-control"
-				ref="text"
-				value={this.state.comment}
-				onChange={v => this.setState({comment: v})}
-				/>
-		    </Validate>
-		    <input
-			    className="btn btn-success"
-			    disabled={!this.state.comment}
-			    type="submit" />
-		</div>
-	    </form>
-	);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {comment: this._getComment()};
+  }
 
-    _onSubmit = (e) => {
-	e.preventDefault();
-	this.props.onSubmit(this.state.comment);
-	this.setState({comment: ""});
-    }
+  _getComment() {
+    let comment = new schema.ManagedObject(schema.string({isRequired: true}));
+    // TODO hacks here to manually call validate, !!NEED to fix ManagedObject constructor!!
+    comment.managed.set('');
+    comment._isBound = {};
+    comment._validate();
+    comment._onChange = () => this.forceUpdate();
+    //////////////////////////////////////////////////
+    return comment;
+  }
+
+  render () {
+    let errors = this.state.comment.managed.errors;
+    return (
+      <form onSubmit={this._onSubmit}>
+        <div
+              className={controlRowErrorClassNames(errors, {'form-group': true})}
+              >
+          <label
+                htmlFor='comment'
+                className='control-label'
+                >
+            {'Comment'}
+          </label>
+          <div style={{position: 'relative'}}>
+	    <DelayedInput
+                        id='comment'
+                        immediate={this.state.comment.managed.isBound}
+			onChange={v => this.state.comment.managed.set(v)}
+                        onPendingChange={v => this.state.comment.managed.pend().set(v)}
+		        >
+	      <textarea
+className="form-control"
+ref="text"
+value={this.state.comment.managed.getPendingOrCurrent()}
+	      />
+	    </DelayedInput>
+            <ControlValidationIcon controlID='comment' errors={errors} />
+          </div>
+          <ControlErrors errors={errors} />
+        </div>
+	<input
+className="btn btn-success"
+disabled={!this.state.comment.isValid() || !this.state.comment.managed.isBound}
+type="submit" />
+      </form>
+    );
+  }
+
+  _onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.comment.managed.get());
+    this.setState({comment: this._getComment()});
+  }
 }
